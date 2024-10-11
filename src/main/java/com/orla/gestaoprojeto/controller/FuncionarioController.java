c129530e57beba493988c9e028665bd97effc556
Rodrigo Silveira <rnsilveira22@gmail.com>
2024-10-11 01:25:44 -0300

    Ref 002 - Bug FuncionarioController criaFuncionario

--- src/main/java/com/orla/gestaoprojeto/controller/FuncionarioController.java
+++ src/main/java/com/orla/gestaoprojeto/controller/FuncionarioController.java
@@ -1,6 +1,7 @@
 package com.orla.gestaoprojeto.controller;
 
 import com.orla.gestaoprojeto.model.Funcionario;
+import com.orla.gestaoprojeto.model.Projeto;
 import com.orla.gestaoprojeto.service.FuncionarioService;
 import com.orla.gestaoprojeto.service.ProjetoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/funcionario")
@@ -26,10 +29,25 @@ public class FuncionarioController {
     }
 
     @PostMapping
-    public ResponseEntity criarFuncionario(@RequestBody Funcionario funcionarioRec) {
-        Funcionario funcResult = new Funcionario(funcionarioRec.getNome());
+    public ResponseEntity<Funcionario> criarFuncionario(@RequestBody Funcionario funcionarioRec) {
+        try {
+            Funcionario funcResult = new Funcionario(funcionarioRec.getNome());
+            Optional.of(funcionarioRec.getCpf()).ifPresent(funcResult::setCpf);
+            Optional.of(funcionarioRec.getSalario()).ifPresent(funcResult::setSalario);
+            Optional.ofNullable(funcionarioRec.getEmail()).ifPresent(funcResult::setEmail);
 
-        return new ResponseEntity<>(funcResult, HttpStatus.CREATED);
+            Set<Projeto> projetoSet = funcionarioRec.getProjetos()
+                    .stream()
+                    .map(this::montaObjetoProjeto)
+                    .filter(projeto -> projeto.getNome() != null && !projeto.getNome().isBlank())
+                    .collect(Collectors.toSet());
+            if (!projetoSet.isEmpty()) funcResult.setProjetos(projetoSet);
+            funcionarioService.criarFuncionario(funcResult);
+            return new ResponseEntity<>(funcResult, HttpStatus.CREATED);
+        } catch (Exception err) {
+            err.printStackTrace();
+            return new ResponseEntity("Erro ao processar a requisição: " + err.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping
@@ -42,4 +60,13 @@ public class FuncionarioController {
         Optional<Funcionario> funcionario = funcionarioService.lerFuncionario(id);
         return funcionario.map(ResponseEntity::ok).orElseGet(()-> ResponseEntity.notFound().build());
     }
+
+    private Projeto montaObjetoProjeto(Projeto projetoDto) {
+        Projeto projetoResult = new Projeto(projetoDto.getNome());
+        projetoResult.setNome(projetoDto.getNome());
+        projetoResult.setDataCriacao(projetoDto.getDataCriacao());
+        projetoService.criarProjeto(projetoResult);
+        return projetoResult;
+    }
+
 }
